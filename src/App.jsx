2d764c78122d17dd3c175ec66b123b8a1a9a4f00
Alex_Bellamy <alexlbellamy@gmail.com>
2020-09-09 15:38:46 +0200

    installing axios, get request for shows, mapping over them to display content

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,28 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Header from "./components/Header"
 import Footer from "./components/Footer"
+import axios from "axios"
 
 const App = () => {
+  const [shows, setShows] = useState([]);
+
+  useEffect(() => {
+    getShows();
+  }, []);
+
+  const getShows = async () => {
+    let response = await axios.get("https://content.viaplay.se/pc-se/serier/samtliga");
+    setShows(response.data._embedded['viaplay:blocks'][0]._embedded['viaplay:products'])
+  }
+
+  let content = shows.map((show) => (
+        <img src={`${show.content.images.landscape.url}`}/>
+    )
+  )
   return (
     <div>
       <Header />
+      <div>{content}</div>
       <Footer />
     </div>
   );
