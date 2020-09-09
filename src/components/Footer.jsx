1a75bdd7cec04593f0847c83689a14269795852e
Alex_Bellamy <alexlbellamy@gmail.com>
2020-09-09 14:32:58 +0200

    creating footer component and importing it into app.jsx, styling it and changing background colour

---
+++ src/components/Footer.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import { Container } from "semantic-ui-react";
+
+const Footer = () => {
+  return (
+    <Container id="footer">
+    </Container>
+  );
+};
+
+export default Footer
